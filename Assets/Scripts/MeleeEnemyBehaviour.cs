2586d9505ad8c8dae159cb169cf1febca28c685e
pmetcalf2 <pmetcalf@mail.bradley.edu>
2022-09-21 10:33:45 -0500

    Melee Enemy

---
+++ Assets/Scripts/MeleeEnemyBehaviour.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleeEnemyBehaviour : MonoBehaviour
+{
+
+    public GameObject player;
+    public float moveSpeed = 3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = FindObjectOfType<PlayerBehaviour>().gameObject;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        FollowPlayer();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            Destroy(gameObject);
+
+        }
+    }
+
+    void FollowPlayer()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, player.transform.position, moveSpeed * Time.deltaTime);
+    }
+}
