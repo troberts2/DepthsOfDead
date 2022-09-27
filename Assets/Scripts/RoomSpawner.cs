2dc3238a464caae99aed12f7e492eacfd754d924
pmetcalf2 <pmetcalf@mail.bradley.edu>
2022-09-26 22:13:55 -0500

    DoorPrefab
    doorprefab made and player scripts added

--- Assets/Scripts/RoomSpawner.cs
+++ Assets/Scripts/RoomSpawner.cs
@@ -8,6 +8,13 @@ public class RoomSpawner : MonoBehaviour
 
     public int currentScene;
 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            ScenePickRandom();
+        }
+    }
     public void ScenePickRandom()
     {
         int index = Random.Range(1, 5);
